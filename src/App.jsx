b1ed579a4b1dd8f9070e57a1f83af4a5465d446b
osemin12-lab <osemin12@gmail.com>
2026-07-31 23:57:10 +0900

    지도추가

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,9 @@ import './App.css'
 import Header from './components/Header'
 import Countdown from './components/Countdown'
 import Invitation from './components/Invitation'
-import Location from './components/Location'
 import Gallery from './components/Gallery'
 import AccountInfo from './components/AccountInfo'
+import Location from './components/Location' // 1. 불러오기 확인
 
 function App() {
   return (
@@ -13,6 +13,7 @@ function App() {
       <Header />
       <Invitation />
       <Countdown />
+      <Location /> {/* 2. 배치가 되어있는지 확인 */}
       <Gallery />
       <AccountInfo />
     </div>
