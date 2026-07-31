d7363c2ff41b5b981cb07785a09d4d19056e51a4
osemin12-lab <osemin12@gmail.com>
2026-07-31 23:15:32 +0900

    다시수정

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React from 'react'
 import './App.css'
 import Header from './components/Header'
 import Countdown from './components/Countdown'
@@ -9,16 +7,14 @@ import Gallery from './components/Gallery'
 import AccountInfo from './components/AccountInfo'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <div>
-      <Header/>
-      <Invitation/>
-      <Countdown/>
-      <Gallery/>
-      <AccountInfo/>
-      </div>
+      <Header />
+      <Invitation />
+      <Countdown />
+      <Gallery />
+      <AccountInfo />
+    </div>
   )
 }
 
