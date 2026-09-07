115e1b833cc75cb94eaad53a6caea3b4c013d72c
osemin12-lab <osemin12@gmail.com>
2026-09-07 11:52:43 +0900

    헤더수정

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,6 +7,10 @@ function Header() {
         <p className="date-line">26 | 10 | 24 | 17:00</p>
         <p className="day">오세민 🖤 이지현</p>
         <p className="datetime">창원 힐스카이웨딩&컨벤션 10층 힐그랜드</p>
+
+        {/* 텍스트 아래 세로 막대기 및 문구 추가 */}
+        <div className="divider"></div>
+        <p className="sub-text">우리 결혼합니다</p>
       </div>
 
       <div className="header-image">
