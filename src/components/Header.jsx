1c2880fe6167dc117aa6f29fe2a1e010476eea0a
osemin12-lab <osemin12@gmail.com>
2026-09-07 10:53:56 +0900

    헤더수정

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,9 +4,9 @@ function Header() {
   return (
     <header className="header-wrapper">
       <div className="header-content">
-        <p className="date-line">26 | 10 | 24 |</p>
+        <p className="date-line">26 | 10 | 24 | 17:00</p>
         <p className="day">오세민 🖤 이지현</p>
-        <p className="datetime">창원 힐스카이웨딩&컨벤션 10층 힐그랜드 17:00</p>
+        <p className="datetime">창원 힐스카이웨딩&컨벤션 10층 힐그랜드</p>
       </div>
 
       <div className="header-image">
